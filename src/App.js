4d268bf0ecb6c4e4c331748d9b9cf5db8c755da8
Paul Liu <oucl5976@gmail.com>
2021-02-23 21:54:34 +0000

    styled-components

--- src/App.js
+++ src/App.js
@@ -3,23 +3,49 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import {Link, Switch, Route} from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavStyles = styled.nav`
+  ul {
+    margin: 20px 0;
+    padding: 0;
+  }
+  
+  li{
+    margin: 0 10px 0 0;
+    padding: 0;
+    list-style: none;
+    display: inline-block;
+  }
+`;
+
+const LinkStyles = styled(Link)`
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  background: palevioletred;
+  color: white;
+  padding: 0.25em 1em;
+  font-weight: bold;
+  font-size: 1.2em;
+  text-decoration: none;
+`;
 
 function App() {
   return (
     <>
-      <nav>
+      <NavStyles>
         <ul>
           <li>
-            <Link  to="/">Home</Link>
+            <LinkStyles  to="/">Home</LinkStyles>
           </li>
           <li>
-            <Link  to="/about">About</Link>
+            <LinkStyles  to="/about">About</LinkStyles>
           </li>
           <li>
-            <Link  to="/contact">Contact</Link>
+            <LinkStyles  to="/contact">Contact</LinkStyles>
           </li>
         </ul>
-      </nav>
+      </NavStyles>
       <Switch>
         <Route exact path="/">
           <Home />
